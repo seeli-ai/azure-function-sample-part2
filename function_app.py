79a69908c3a14a588d1bc40c5583ce63b20afb03
Markus Seeli <markus@seeli.eu>
2024-09-21 18:14:05 +0000

    rapid api

--- function_app.py
+++ function_app.py
@@ -1,6 +1,8 @@
 import azure.functions as func
 import logging
 import json
+import http.client
+import os
 
 app = func.FunctionApp()
 
@@ -14,13 +16,41 @@ def do_translation(msg: func.QueueMessage, outputTable: func.Out[str]) -> None:
         # Parse the queue message
         message_content = msg.get_body().decode('utf-8')
         message_json = json.loads(message_content)
+        text = message_json.get('text', '')
+        from_language = message_json.get('from', 'en')
+        to_language = message_json.get('to', 'de')
+
+        to_translate = {
+            "from": from_language,
+            "to": to_language,
+            "text": text
+        }
+
+        conn = http.client.HTTPSConnection("google-translate113.p.rapidapi.com")
+
+        payload = json.dumps(to_translate)
+
+        headers = {
+            'x-rapidapi-key': os.environ['RAPIDAPI_KEY'],
+            'x-rapidapi-host': "google-translate113.p.rapidapi.com",
+            'Content-Type': "application/json"
+        }
+
+        conn.request("POST", "/api/v1/translator/text", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+
+        translation = json.loads(data.decode("utf-8"))['trans']
+
 
         # Prepare the entity to be inserted
         entity = {
             "PartitionKey": message_json.get('user', 'user'),
             "RowKey": message_json.get('id', str(msg.id)),
-            "OriginalMessage": message_json.get('text', ''),
-            "TranslatedMessage": "Not translated yet"  # Placeholder for future translation
+            "From": from_language,
+            "To": to_language,
+            "OriginalText": message_json.get('text', ''),
+            "TranslatedText": translation
         }
 
         # Insert the entity into the table
